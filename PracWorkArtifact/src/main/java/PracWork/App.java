ffea9e3bfbb0f09859bf70bf68bb85506ec71fb7
honey <xyz>
2021-05-26 21:16:05 +0000

    Changes to Java Prograam

--- PracWorkArtifact/src/main/java/PracWork/App.java
+++ PracWorkArtifact/src/main/java/PracWork/App.java
@@ -1,13 +1,21 @@
 package PracWork;
 
 /**
- * Hello world!
+ * Adding two integers
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+class Main {
+
+  public static void main(String[] args) {
+
+    System.out.println("Enter two numbers");
+    int first = 10;
+    int second = 20;
+
+    System.out.println(first + " " + second);
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println("The sum is: " + sum);
+  }
 }
